9c09494efdc750ed4c3fe3fe677e591f231edbc9
danielvschoor <danielvanschoor1997@gmail.com>
2024-01-03 14:33:41 +0200

    feat: Add loggin

--- AiarenaCachingServer/Controllers/DownloadController.cs
+++ AiarenaCachingServer/Controllers/DownloadController.cs
@@ -19,13 +19,15 @@ public class DownloadController(
     [HttpPost]
     public async Task<IActionResult> Download([FromBody] DownloadRequest downloadRequest)
     {
+        logger.LogInformation("Received request: {UniqueKey}, {Md5Hash}", downloadRequest.UniqueKey, downloadRequest.Md5Hash);
         if (cachingSingleton.CachingMap.TryGetValue(downloadRequest.UniqueKey, out CacheObject? cacheObject) &&
             cacheObject.Md5Hash == downloadRequest.Md5Hash && Path.Exists(cacheObject.Path))
         {
+            logger.LogInformation("Cache exists for {Md5Hash}", downloadRequest.Md5Hash);
             var fileStream = System.IO.File.OpenRead(cacheObject.Path);
             return new FileStreamResult(fileStream, "application/octet-stream");
         }
-
+        logger.LogInformation("Cache does not exist for {Md5Hash}", downloadRequest.Md5Hash);
         if (cacheObject != null && Path.Exists(cacheObject.Path))
         {
             System.IO.File.Delete(cacheObject.Path);
@@ -48,13 +50,15 @@ public class DownloadController(
         }
 
         response.EnsureSuccessStatusCode();
-
-        var file = await response.Content.ReadAsStreamAsync();
+        
 
         var downloadDirectory = configuration.GetValue<string>("DownloadDirectory")!;
 
         Directory.CreateDirectory(downloadDirectory);
+        
         var newPath = Path.Join(downloadDirectory, downloadRequest.Md5Hash);
+        
+        var file = await response.Content.ReadAsStreamAsync();
         
         await using (var fileStream = System.IO.File.Create(newPath))
         {
@@ -67,6 +71,7 @@ public class DownloadController(
             Md5Hash = downloadRequest.Md5Hash,
             Path = newPath
         });
+        
         file.Seek(0, SeekOrigin.Begin);
         return new FileStreamResult(file, "application/octet-stream");
     }
