0d2156a2b700b33b287c582ea6e64723be6858ab
danielvschoor <danielvanschoor1997@gmail.com>
2024-01-03 15:37:21 +0200

    fix: Change cache map TryAdd to AddOrUpdate

--- AiarenaCachingServer/Controllers/DownloadController.cs
+++ AiarenaCachingServer/Controllers/DownloadController.cs
@@ -10,16 +10,16 @@ public class DownloadController(
     IConfiguration configuration,
     ILogger<DownloadController> logger) : Controller
 {
-
     private string AuthHeader()
     {
         return $"Token {configuration.GetValue<string>("ApiToken")}";
     }
-    
+
     [HttpPost]
     public async Task<IActionResult> Download([FromBody] DownloadRequest downloadRequest)
     {
-        logger.LogInformation("Received request: {UniqueKey}, {Md5Hash}", downloadRequest.UniqueKey, downloadRequest.Md5Hash);
+        logger.LogInformation("Received request: {UniqueKey}, {Md5Hash}", downloadRequest.UniqueKey,
+            downloadRequest.Md5Hash);
         if (cachingSingleton.CachingMap.TryGetValue(downloadRequest.UniqueKey, out CacheObject? cacheObject) &&
             cacheObject.Md5Hash == downloadRequest.Md5Hash && Path.Exists(cacheObject.Path))
         {
@@ -27,12 +27,13 @@ public class DownloadController(
             var fileStream = System.IO.File.OpenRead(cacheObject.Path);
             return new FileStreamResult(fileStream, "application/octet-stream");
         }
+
         logger.LogInformation("Cache does not exist for {Md5Hash}", downloadRequest.Md5Hash);
         if (cacheObject != null && Path.Exists(cacheObject.Path))
         {
             System.IO.File.Delete(cacheObject.Path);
         }
-        
+
         var url = downloadRequest.Url;
         httpClient.DefaultRequestHeaders.Remove("Authorization");
         if (url.Contains("aiarena.net"))
@@ -50,28 +51,32 @@ public class DownloadController(
         }
 
         response.EnsureSuccessStatusCode();
-        
+
 
         var downloadDirectory = configuration.GetValue<string>("DownloadDirectory")!;
 
         Directory.CreateDirectory(downloadDirectory);
-        
+
         var newPath = Path.Join(downloadDirectory, downloadRequest.Md5Hash);
-        
+
         var file = await response.Content.ReadAsStreamAsync();
-        
+
         await using (var fileStream = System.IO.File.Create(newPath))
         {
             file.Seek(0, SeekOrigin.Begin);
             await file.CopyToAsync(fileStream);
             fileStream.Close();
         }
-        cachingSingleton.CachingMap.TryAdd(downloadRequest.UniqueKey, new CacheObject()
+
+        var newCacheObject = new CacheObject()
         {
             Md5Hash = downloadRequest.Md5Hash,
             Path = newPath
-        });
+        };
         
+        cachingSingleton.CachingMap.AddOrUpdate(downloadRequest.UniqueKey, newCacheObject, (_, _) => newCacheObject);
+
+
         file.Seek(0, SeekOrigin.Begin);
         return new FileStreamResult(file, "application/octet-stream");
     }
